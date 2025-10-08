ffadfddbe7036de2597d0901a85e05e24e790558
Hegdeniteesh <niteeshhegde1721@gmail.com>
2025-10-08 11:10:17 +0530

    add registration and login routes

---
+++ frontend/src/components/Layout/AuthLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const AuthLayout = () => {
+    return (
+        <div className="auth-layout">
+            <div className="auth-container">
+                <div className="auth-header">
+                    <Link to="/" className="auth-brand">🚀 WorkWise</Link>
+                </div>
+                {/* The Login or Register page will be rendered here */}
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default AuthLayout;
